0bc1315b5534ab70e738c36435c5edfa50cb7d09
gamergiorgi339@gmail.com <gamergiogi339@gmail.com>
2024-03-23 19:56:19 +0400

    .

---
+++ day33 js/day32(2).js
@@ -0,0 +1,17 @@
+
+const birds = 6;
+const nestsPerBird = 2;
+const chicksPerNest = 3;
+const eggsPerNest = 3;
+
+
+const totalNests = birds * nestsPerBird;
+
+
+const totalEggs = totalNests * eggsPerNest;
+
+
+const totalBats = totalEggs;
+
+
+console.log("Total bats hatching:", totalBats);
---
+++ day33 js/day33.js
@@ -0,0 +1,19 @@
+const familyMembers = Object.freeze({
+    member1: {
+      name: "kakha",
+      age: 49,
+      ageIn20Years: 49 + 20
+    },
+    member2: {
+      name: "sofi",
+      age: 40,
+      ageIn20Years: 40 + 20
+    },
+    member3: {
+      name: "mari",
+      age: 18,
+      ageIn20Years: 18 + 20
+    }
+  });
+  
+  console.log(familyMembers);
