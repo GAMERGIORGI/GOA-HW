4942fbf793020ca229fff74382881f3b0932c93b
gamergiorgi339@gmail.com <gamergiogi339@gmail.com>
2024-05-21 19:56:12 +0400

    .

--- JAVA SCRIPT/day 71/script.js
+++ JAVA SCRIPT/day 71/script.js
@@ -3,6 +3,7 @@ const incrementButton = document.getElementById('increment');
 const decrementButton = document.getElementById('decrement');
 const resetButton = document.getElementById('reset');
 
+
 let count = 0;
 
 
@@ -10,18 +11,14 @@ function updateCount() {
     countElement.textContent = count;
 }
 
-
 incrementButton.addEventListener('click', () => {
     count++;
     updateCount();
 });
 
-
 decrementButton.addEventListener('click', () => {
-    if (count > 0) {
-        count--;
-        updateCount();
-    }
+    count--;
+    updateCount();
 });
 
 
@@ -29,5 +26,6 @@ resetButton.addEventListener('click', () => {
     count = 0;
     updateCount();
 });
+
 
 updateCount();
